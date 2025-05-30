e4ace2a91ee5db52dcd41d7661bc8a302a82f274
Tschongsti <lukas.angster@gmail.com>
2025-05-30 07:37:13 +0200

    Stand nach Abschluss des Kapitels 13 des Udemy-Kurses

--- lib/screens/places.dart
+++ lib/screens/places.dart
@@ -5,35 +5,57 @@ import 'package:favorite_places/providers/user_places_provider.dart';
 import 'package:favorite_places/screens/add_place.dart';
 import 'package:favorite_places/widgets/places_list.dart';
 
-class PlacesScreen extends ConsumerWidget {
+class PlacesScreen extends ConsumerStatefulWidget {
   const PlacesScreen ({super.key});
 
-@override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final userPlaces = ref.watch(userPlacesProvider);
-    
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Your Places'),
-        actions: [
-          IconButton(
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (ctx) => const AddPlaceScreen()
-                ),
-              );
-            },
-            icon: const Icon(Icons.add),
-          ),
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8),
-        child: PlacesList(
-          places: userPlaces
-        ),
-      ),
-    );
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() {
+    return _PlacesScreenState();
   }
+
 }
+
+class _PlacesScreenState extends ConsumerState<PlacesScreen> {
+  late Future<void> _placesFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _placesFuture = ref.read(userPlacesProvider.notifier).loadPlaces();
+  }
+
+  @override
+    Widget build(BuildContext context) {
+      final userPlaces = ref.watch(userPlacesProvider);
+      
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Your Places'),
+          actions: [
+            IconButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (ctx) => const AddPlaceScreen()
+                  ),
+                );
+              },
+              icon: const Icon(Icons.add),
+            ),
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8),
+          child: FutureBuilder(
+            future: _placesFuture,
+            builder: (context, snapshot) =>
+              snapshot.connectionState == ConnectionState.waiting
+                ? const Center(child: CircularProgressIndicator())
+                : PlacesList(
+                    places: userPlaces,
+                ),
+          ),
+        ),
+      );
+    }
+  }
